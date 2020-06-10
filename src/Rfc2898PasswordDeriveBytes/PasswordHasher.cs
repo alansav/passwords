f2474eec8492537ad1e63dfe77cb64f8aa99fa1d
Alan Savage <17855829+alansav@users.noreply.github.com>
2020-06-10 17:57:01 +0100

    Initial commit

---
+++ src/Rfc2898PasswordDeriveBytes/PasswordHasher.cs
@@ -0,0 +1,73 @@
+﻿using Savage.Providers;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Savage.Passwords.Rfc2898PasswordDeriveBytes
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string AlgorithmName { get => "rfc2898"; }
+
+        public string Hash(byte[] password, int iterations = 1024, int saltSize = 16)
+        {
+            var salt = GenerateSalt(saltSize);
+            
+            var computedHash = ComputeHash(password, salt, iterations);
+
+            return $"${AlgorithmName}${Convert.ToBase64String(salt)}${iterations}${Convert.ToBase64String(computedHash)}";
+        }
+
+        public string Hash(string password, int iterations = 1024, int saltSize = 16)
+        {
+            var passwordAsBytes = Encoding.UTF8.GetBytes(password);
+            return Hash(passwordAsBytes, iterations, saltSize);
+        }
+
+        public bool Compare(string passwordHash, byte[] password)
+        {
+            var elements = passwordHash.Split('$');
+            if (elements.Length != 5)
+            {
+                throw new ArgumentException($"Unable to parse: {nameof(passwordHash)}");
+            }
+            var hashingAlgorithm = elements[1];
+            if (hashingAlgorithm != AlgorithmName)
+            {
+                throw new ArgumentException($"The algorithm used to hash the password does not match the expected algorithm: {AlgorithmName}");
+            }
+
+            var salt = Convert.FromBase64String(elements[2]);
+            var iterations = int.Parse(elements[3]);
+            var hashedPassword = Convert.FromBase64String(elements[4]);
+
+            var computedHash = ComputeHash(password, salt, iterations);
+            return hashedPassword.SequenceEqual(computedHash);
+        }
+
+        public bool Compare(string passwordHash, string password)
+        {
+            var passwordAsBytes = Encoding.UTF8.GetBytes(password);
+            return Compare(passwordHash, passwordAsBytes);
+        }
+
+        private byte[] ComputeHash(byte[] password, byte[] salt, int iterations)
+        {
+            using (var passwordDeriveBytes = new Rfc2898DeriveBytes(password, salt, iterations))
+            {
+                var key = passwordDeriveBytes.GetBytes(256);
+                using (var hasher = SHA512.Create())
+                {
+                    return hasher.ComputeHash(key);
+                }
+            }
+        }
+
+        private byte[] GenerateSalt(int saltSize)
+        {
+            var randomBytesProvider = new RandomBytesProvider();
+            return randomBytesProvider.GetBytes(saltSize);
+        }
+    }
+}
---
+++ src/UnitTests/PasswordHasherTests.cs
@@ -0,0 +1,85 @@
+﻿using Savage.Passwords;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UnitTests
+{
+    public class PasswordHasherTests
+    {
+        public class PasswordHashers : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new[] { new Savage.Passwords.Rfc2898PasswordDeriveBytes.PasswordHasher() };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+        
+        [Theory]
+        [ClassData(typeof(PasswordHashers))]
+        public void Hash_returns_different_values_when_called_with_same_password(IPasswordHasher sut)
+        {
+            const string password = "password";
+
+            var hash1 = sut.Hash(password);
+            var hash2 = sut.Hash(password);
+
+            Assert.NotEqual(hash1, hash2);
+        }
+
+        [Theory]
+        [ClassData(typeof(PasswordHashers))]
+        public void Compare_password_returns_true_when_password_is_same_as_password_used_to_hash(IPasswordHasher sut)
+        {
+            const string password = "password";
+
+            var passwordHash = sut.Hash(password);
+
+            var match = sut.Compare(passwordHash, password);
+
+            Assert.True(match);
+        }
+
+        [Theory]
+        [ClassData(typeof(PasswordHashers))]
+        public void Compare_password_returns_false_when_password_is_different_to_password_used_to_hash(IPasswordHasher sut)
+        {
+            const string password = "password";
+
+            var passwordHash = sut.Hash(password);
+
+            var match = sut.Compare(passwordHash, "wrong");
+
+            Assert.False(match);
+        }
+
+        [Theory]
+        [ClassData(typeof(PasswordHashers))]
+        public void Compare_throws_ArgumentException_when_passwordHash_contains_3_separators(IPasswordHasher sut)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => sut.Compare("$2$3$4", "password"));
+            Assert.Equal("Unable to parse: passwordHash", ex.Message);
+        }
+
+        [Theory]
+        [ClassData(typeof(PasswordHashers))]
+        public void Compare_throws_ArgumentException_when_passwordHash_contains_5_separators(IPasswordHasher sut)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => sut.Compare("$2$3$4$5$6", "password"));
+            Assert.Equal("Unable to parse: passwordHash", ex.Message);
+        }
+
+        [Theory]
+        [ClassData(typeof(PasswordHashers))]
+        public void Compare_throws_ArgumentException_when_first_element_is_not_valid(IPasswordHasher sut)
+        {
+            var passwordHash = "$invalid$3$4$5";
+
+            var ex = Assert.Throws<ArgumentException>(() => sut.Compare(passwordHash, "password"));
+            Assert.Equal("The algorithm used to hash the password does not match the expected algorithm: rfc2898", ex.Message);
+        }
+    }
+}
